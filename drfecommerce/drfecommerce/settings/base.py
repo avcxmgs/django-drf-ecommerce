3399570c9e38bac83f0c5a1ebbcbd6f3d68cdd10
Margareth Santos <maggie_snts@yahoo.ca>
2024-05-03 14:49:23 -0500

    added rest framework to base.py

--- drfecommerce/drfecommerce/settings/base.py
+++ drfecommerce/drfecommerce/settings/base.py
@@ -17,6 +17,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #External Packages
+    'rest_framework',
+    #Internal Apps
 ]
 
 MIDDLEWARE = [
@@ -75,3 +78,5 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {}
